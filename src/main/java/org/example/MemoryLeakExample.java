95074038c8b04c7967fa1ac4ec8f5c655b1dd180
Viner108 <andrukovichl@mail.ru>
2024-02-04 18:13:44 +0300

    String memory

--- src/main/java/org/example/MemoryLeakExample.java
+++ src/main/java/org/example/MemoryLeakExample.java
@@ -2,17 +2,29 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MemoryLeakExample {
     static class Data {
-        byte[] memoryConsumer = new byte[1024 + 1024];
+
+        Long[] list;
+
+        public Data(Long[] list) {
+            this.list = list;
+        }
+
+        String memory = "Hello Memory";
     }
 
     public static void main(String[] args) throws InterruptedException {
         List<Data> list = new ArrayList<>();
+        int i = 0;
         while (true) {
-            list.add(new Data());
-            Thread.sleep(50);
+            list.add(new Data(new Long[1]));
+//            i++;
+//            if (i % 10000 == 0) {
+//                System.out.println(i);
+//            }
         }
     }
 }
